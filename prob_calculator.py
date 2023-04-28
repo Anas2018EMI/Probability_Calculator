67a135a3da3a51ba87cb6c2b9a036b1f4bcdb434
Anas2018EMI <anassaatef@gmail.com>
2023-04-28 17:02:11 +0000

    Version 1.0.0

--- prob_calculator.py
+++ prob_calculator.py
@@ -68,5 +68,5 @@ print(hat.contents)
 probability = experiment(hat=hat, expected_balls={
                          "blue": 2, "green": 1}, num_balls_drawn=4, num_experiments=1000)
 
-
+#
 print(probability)
